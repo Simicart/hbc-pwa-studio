1c800f806bff9f3faf88cea90f579fd7631130ac
hung <quochung@simicart.com>
2020-09-15 14:50:24 +0700

    update style

--- packages/siminia/src/simi/App/hairbowcenter/BaseComponents/Products/SubCategories/index.js
+++ packages/siminia/src/simi/App/hairbowcenter/BaseComponents/Products/SubCategories/index.js
@@ -17,13 +17,14 @@ const SubCategories = (props) => {
 
     const renderSubCate = (child_cats) => {
         const cateHtml = child_cats.map((child, ix) => {
-            const thumbnailUrl = child.thumbnail ? resourceUrl(child.thumbnail, { type: 'image-category' }) : require('./../../../Images/placeholder.svg') ;
+         
+            const thumbnailUrl = child.thumbnail ? resourceUrl(child.thumbnail, { type: 'image-category' }) : '/icons/placeholder.svg' ;
             const subCate = child.child_cats && child.child_cats.length ? child.child_cats.map((subC, idx) => {
                 return <li><Link key={ix+idx} to={'/' + subC.url_path} title={subC.name} className="category-name child-category">{subC.name}</Link></li>
             }) : '';
             return <li className="item" key={ix}>
                 <Link to={'/' + child.url_path} title={child.name}>
-                    <Image src={thumbnailUrl} />
+                    <Image src={thumbnailUrl} width={200} heigth={200}/>
                 </Link>
                 <div className="category-name parent-category">
                     <Link to={'/' + child.url_path}>{child.name}</Link>
